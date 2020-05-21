30c2763221c61bf5a8b2e99c41a411d25796fb9d
gfraser27 <graeme.fraser@cgu.edu>
2020-05-21 14:10:11 -0700

    added problem 2

--- BCE_2_2.py
+++ BCE_2_2.py
@@ -1,3 +1,5 @@
+
+# Problem 1 #
 x = .016
 y = 1000000
 z = x * y
@@ -5,3 +7,7 @@ y = str(y)
 z = str(z)
 print(y + ' ' + 'Bolivar is equivalent to ' + ' ' + z + ' U.S. dollars.')
 
+# Problem 2 #
+l = 12060
+k = (l/x) * 3
+print(k)
