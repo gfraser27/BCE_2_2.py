153da072a5f9e6a84de9ef3c690c5d7023b47018
gfraser27 <graeme.fraser@cgu.edu>
2020-05-21 18:16:50 -0700

    team exercise

--- BCE_2_2.py
+++ BCE_2_2.py
@@ -11,3 +11,36 @@ print(y + ' ' + 'Bolivar is equivalent to ' + ' ' + z + ' U.S. dollars.')
 l = 12060
 k = (l/x) * 3
 print(k)
+
+# Problem 3 #
+p = 365.2425 * 3
+q = 24 
+r = p * q * 60
+print(r)
+
+s = k/r
+t = k//r
+print(s)
+print(t)
+
+# Problem 4 #
+bloody_vikings = "Wonderful Spam! Glorious Spam!"
+
+# Problem 5 #
+viking_exclamations = ["Wonderful Spam!" ,"Glorious Spam!" ]
+
+# Problem 6 #
+viking_exaggeration = viking_exclamations[-1]
+print(viking_exaggeration)
+
+# Problem 7 #
+upper_exaggeration = ''
+for letter in viking_exaggeration :
+    letter = letter.capitalize()
+    upper_exaggeration = upper_exaggeration + letter
+print(upper_exaggeration)
+
+# Problem 8 #
+partial_exaggeration = viking_exaggeration[4:9]
+print(partial_exaggeration)
+
